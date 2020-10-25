74718fe399539e5a2fa5a4c209405b5fe6ab3daf
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-10-25 19:19:35 +0300

    script file

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#play").click(function(){
+      $("#section1").hide("slow");
+    });
+    $("#play").click(function(){
+      $("table").show("slow");
+    });
+});
